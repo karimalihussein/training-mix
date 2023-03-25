6284bb440c533fa1394b00f282a3957dc32248a3
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-03-25 03:41:47 +0200

    Service Container in advanced ways with custom Container and tests

---
+++ app/Containers/Container.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Containers;
+
+use Closure;
+use Exception;
+use ReflectionClass;
+use ReflectionNamedType;
+use ReflectionParameter;
+
+class Container
+{
+    protected array $bindings = [];
+    protected array $singletons = [];
+
+    /**
+     * Binds a given abstract to a concrete implementation or closure.
+     *
+     * @param string $abstract
+     * @param string|Closure $concrete
+     * @param bool $shared
+     * @return void
+     */
+    public function bind(string $abstract, string|Closure $concrete, bool $shared = false): void
+    {
+        $this->bindings[$abstract] = compact('concrete', 'shared');
+    }
+
+    /**
+     * Binds a given abstract to a concrete implementation or closure as a singleton.
+     *
+     * @param string $abstract
+     * @param string|Closure $concrete
+     * @return void
+     */
+    public function singleton(string $abstract, string|Closure $concrete): void
+    {
+        $this->bind($abstract, $concrete, true);
+    }
+
+    /**
+     * Resolves the given abstract out of the container.
+     *
+     * @param string $abstract
+     * @return mixed
+     * @throws Exception
+     */
+    public function get(string $abstract): mixed
+    {
+        if (!isset($this->bindings[$abstract])) {
+            if (class_exists($abstract)) {
+                return $this->resolveClass($abstract);
+            }
+            throw new Exception("No binding found for {$abstract}");
+        }
+
+        $binding = $this->bindings[$abstract];
+
+        if ($binding['shared'] && isset($this->singletons[$abstract])) {
+            return $this->singletons[$abstract];
+        }
+
+        $concrete = $binding['concrete'];
+
+        if (!$concrete instanceof Closure) {
+            return $concrete;
+        }
+
+        $object = $concrete($this);
+
+        if ($binding['shared']) {
+            $this->singletons[$abstract] = $object;
+        }
+
+        return $object;
+    }
+
+    /**
+     * Resolves the given class out of the container.
+     *
+     * @param string $class
+     * @return mixed
+     * @throws Exception
+     */
+    protected function resolveClass(string $class): mixed
+    {
+        $reflector = new ReflectionClass($class);
+
+        if (!$constructor = $reflector->getConstructor()) {
+            return new $class();
+        }
+
+        $dependencies = $this->resolveDependencies($constructor->getParameters());
+
+        return $reflector->newInstanceArgs($dependencies);
+    }
+
+    /**
+     * Resolves the dependencies for the given array of parameters.
+     *
+     * @param ReflectionParameter[] $parameters
+     * @return array
+     * @throws Exception
+     */
+    protected function resolveDependencies(array $parameters): array
+    {
+        $dependencies = [];
+
+        foreach ($parameters as $parameter) {
+            if ($parameter->isDefaultValueAvailable()) {
+                $dependencies[] = $parameter->getDefaultValue();
+            } else {
+                $dependencies[] = $this->getDependency($parameter);
+            }
+        }
+
+        return $dependencies;
+    }
+
+    /**
+     * Resolves the dependency for the given parameter.
+     *
+     * @param ReflectionParameter $parameter
+     * @return mixed
+     * @throws Exception
+     */
+    protected function getDependency(ReflectionParameter $parameter): mixed
+    {
+        $dependency = $parameter->getType();
+
+        if ($dependency instanceof ReflectionNamedType) {
+            return $this->get($dependency->getName());
+        }
+
+        throw new Exception("Cannot resolve dependency for {$parameter->getName()}");
+    }
+
+}
---
+++ tests/Unit/Bits/ContainersTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Classes\Newsletter;
+use App\Containers\Container;
+use PHPUnit\Framework\TestCase;
+
+class ContainersTest extends TestCase
+{
+    /** @test */
+    function level_one_it_can_bind_keys_values(): void
+    {
+        $container = new Container();
+        $container->bind('foo', 'bar');
+        $this->assertEquals('bar', $container->get('foo'));
+    }
+
+    /** @test */
+    function level_two_it_can_lazily_resolve_functions()
+    {
+        $container = new Container();
+        $container->bind('newsletter', function () {
+            return new Newsletter(uniqid());
+        });
+        $this->assertInstanceOf(Newsletter::class, $container->get('newsletter'));
+    }
+
+    public function testBindAndGetInstance()
+    {
+        $container = new Container();
+        $container->bind('foo', function () {
+            return new Newsletter();
+        });
+
+        $foo1 = $container->get('foo');
+        $foo2 = $container->get('foo');
+
+        $this->assertInstanceOf(Newsletter::class, $foo1);
+        $this->assertInstanceOf(Newsletter::class, $foo2);
+        $this->assertNotSame($foo1, $foo2);
+    }
+
+    public function testSingleton()
+    {
+        $container = new Container();
+
+        $container->singleton('foo', function () {
+            return new Newsletter();
+        });
+
+        $foo1 = $container->get('foo');
+        $foo2 = $container->get('foo');
+
+        $this->assertInstanceOf(Newsletter::class, $foo1);
+        $this->assertInstanceOf(Newsletter::class, $foo2);
+        $this->assertSame($foo1, $foo2);
+    }
+
+
+    
+}

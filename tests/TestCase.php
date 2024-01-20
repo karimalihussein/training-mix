bf60e4a2725abe401fac5e2dbfac63df91df0c8b
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-01-20 23:16:26 +0200

    🛠️refactor(tests) update actingAs method signature in TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Contracts\Auth\Authenticatable as UserContract;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
@@ -14,7 +15,7 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication;
     use LazilyRefreshDatabase;
 
-    public function actingAs(UserContract $user, $abilities = ['*'])
+    public function actingAs(UserContract|User $user, $abilities = ['*'])
     {
         Sanctum::actingAs($user, $abilities);
 

0acda29f370c44f9d974e2a416ebe6e9ee643a8e
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-01-21 20:46:40 +0200

    🚀feat(api) add route to fetch order details
    This commit adds a new route to fetch order details by order ID. The route is accessible at `orders/{order}` and returns the order details along with the associated product information. Additionally, the route is protected with the `auth:sanctum` middleware.

--- modules/Order/api.php
+++ modules/Order/api.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use Modules\Order\Http\Controllers\CheckoutController;
 
 Route::post('checkout', CheckoutController::class)->name('checkout.store')->middleware('auth:sanctum');
+Route::get('orders/{order}', fn (Modules\Order\Models\Order $order) => $order->load('lines.product'))->name('orders.show')->middleware('auth:sanctum');

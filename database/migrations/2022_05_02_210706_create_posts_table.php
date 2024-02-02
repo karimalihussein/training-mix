08d35e7de1e1a0382e5c6405d60e5e20b8a180c1
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-02-02 16:48:01 +0200

    Add PostActiveEnum to create_posts_table migration

--- database/migrations/2022_05_02_210706_create_posts_table.php
+++ database/migrations/2022_05_02_210706_create_posts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\PostActiveEnum;
 use App\Models\Post;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -7,30 +8,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title')->unique();
             $table->text('content');
-            $table->unsignedSmallInteger('active')->default(Post::ACTIVE_STATUS);
+            $table->unsignedSmallInteger('active')->default(PostActiveEnum::ACTIVE->value);
             $table->foreignId('user_id')->index()->constrained('users')->onDelete('cascade');
             $table->softDeletes();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('posts');
     }

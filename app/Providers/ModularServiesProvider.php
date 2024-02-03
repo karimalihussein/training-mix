e6bdf342304bd179539be16c51df4017e101ca55
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-02-03 23:30:25 +0200

    Add BookingServiceProvider to ModularServicesProvider

--- app/Providers/ModularServiesProvider.php
+++ app/Providers/ModularServiesProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Modules\Booking\Providers\BookingServiceProvider;
 use Modules\Order\Providers\OrderServiceProvider;
 use Modules\Payment\Infrastructure\Providers\PaymentServiceProvider;
 use Modules\Product\Providers\ProductServiceProvider;
@@ -18,5 +19,6 @@ final class ModularServiesProvider extends ServiceProvider
         $this->app->register(OrderServiceProvider::class);
         $this->app->register(PaymentServiceProvider::class);
         $this->app->register(ShipmentServiceProvider::class);
+        $this->app->register(BookingServiceProvider::class);
     }
 }

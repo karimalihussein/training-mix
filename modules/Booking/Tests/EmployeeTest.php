e9f36ea5d33d2f11198dba2f77937a99f5ed2eb0
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-02-07 00:52:45 +0200

    Refactor employee availability tests

--- modules/Booking/Tests/EmployeeTest.php
+++ modules/Booking/Tests/EmployeeTest.php
@@ -9,13 +9,11 @@ use Modules\Booking\Models\Schedule;
 use Modules\Booking\Models\ScheduleExclusion;
 use Modules\Booking\Models\Service;
 use Modules\Booking\Support\ScheduleAvailability;
-use Spatie\Period\Period;
 use Spatie\Period\PeriodCollection;
 use Tests\TestCase;
 
 final class EmployeeTest extends TestCase
 {
-
     public function test_an_employee_can_have_many_appointments(): void
     {
         $employee = Employee::factory()->create();
@@ -34,7 +32,7 @@ final class EmployeeTest extends TestCase
 
         $this->assertEquals(3, $employee->services->count());
     }
-    public function test_list_correct_employee_availability(): void
+    public function testListCorrectEmployeeAvailability(): void
     {
         Carbon::setTestNow(Carbon::parse('1st January 2000'));
 
@@ -43,9 +41,11 @@ final class EmployeeTest extends TestCase
                 'starts_at' => now()->startOfDay(),
                 'ends_at' => now()->addYear()->endOfDay(),
             ]))->create();
-        $service = Service::factory()->create([
-            'duration' => 30,
-        ]);
+
+        $service = Service::factory()->create(['duration' => 30]);
+
+        $dayName = strtolower(now()->format('l'));
+        $actualStartTime = $employee->schedules->first()->{$dayName . '_starts_at'};
 
         $availability = (new ScheduleAvailability($employee, $service))->forPeriod(
             now()->startOfDay(),
@@ -53,5 +53,113 @@ final class EmployeeTest extends TestCase
         );
 
         $this->assertInstanceOf(PeriodCollection::class, $availability);
+
+        $availabilityPeriod = $availability->current();
+
+        $expectedStartTime = now()->setTimeFromTimeString($actualStartTime);
+
+        $this->assertTrue($availabilityPeriod->startsAt($expectedStartTime));
+    }
+
+
+    public function test_accounts_for_different_daily_schedule(): void
+    {
+        Carbon::setTestNow(Carbon::parse('monday January 2000'));
+
+        $employee = Employee::factory()
+            ->has(Schedule::factory()->state([
+                'starts_at' => now()->startOfDay(),
+                'ends_at' => now()->addYear()->endOfDay(),
+                'monday_starts_at' => '11:00',
+                'monday_ends_at' => '16:00',
+                'tuesday_starts_at' => '09:00',
+                'tuesday_ends_at' => '17:00',
+            ]))->create();
+
+        $service = Service::factory()->create(['duration' => 30]);
+
+        $availability = (new ScheduleAvailability($employee, $service))->forPeriod(
+            now()->startOfDay(),
+            now()->addDay()->endOfDay()
+        );
+
+        $this->assertInstanceOf(PeriodCollection::class, $availability);
+
+        $availabilityPeriod = $availability->current();
+
+        $this->assertTrue($availabilityPeriod->startsAt(now()->setTimeFromTimeString('11:00')));
+        $this->assertTrue($availabilityPeriod->endsAt(now()->setTimeFromTimeString('15:30')));
+
+        $availability->next();
+
+        $availabilityPeriod = $availability->current();
+
+        $this->assertTrue($availabilityPeriod->startsAt(now()->addDay()->setTimeFromTimeString('09:00')));
+        $this->assertTrue($availabilityPeriod->endsAt(now()->addDay()->setTimeFromTimeString('16:30')));
+    }
+
+
+    public function test_does_not_show_availability_for_schedule_exclusions(): void
+    {
+        Carbon::setTestNow(Carbon::parse('1st January 2000'));
+
+        $employee = Employee::factory()
+            ->has(Schedule::factory()->state([
+                'starts_at' => now()->startOfDay(),
+                'ends_at' => now()->addYear()->endOfDay(),
+            ]))->has(ScheduleExclusion::factory()->state([
+                'starts_at' => now()->addDay()->startOfDay(),
+                'ends_at' => now()->addDay()->endOfDay(),
+            ]))->has(ScheduleExclusion::factory()->state([
+                'starts_at' => now()->setTimeFromTimeString('12:00'),
+                'ends_at' => now()->setTimeFromTimeString('13:00'),
+            ]))->create();
+
+
+
+        $service = Service::factory()->create(['duration' => 30]);
+
+        $availability = (new ScheduleAvailability($employee, $service))->forPeriod(
+            now()->startOfDay(),
+            now()->addDay()->endOfDay()
+        );
+
+        $this->assertInstanceOf(PeriodCollection::class, $availability);
+
+        $availabilityPeriod = $availability->current();
+        $dayName = strtolower(now()->format('l'));
+        $actualStartTime = $employee->schedules->first()->{$dayName . '_starts_at'};
+        $this->assertTrue($availabilityPeriod->startsAt(now()->setTimeFromTimeString($actualStartTime)));
+        $this->assertTrue($availabilityPeriod->endsAt(now()->setTimeFromTimeString('11:59')));
+
+        $availability->next();
+
+        $availabilityPeriod = $availability->current();
+
+        $this->assertTrue($availabilityPeriod->startsAt(now()->setTimeFromTimeString('13:00')));
+    }
+
+    public function test_only_show_availability_from_the_current_time_with_an_hour_in_advanced(): void
+    {
+        Carbon::setTestNow(Carbon::parse('1st January 2000 09:15'));
+
+        $employee = Employee::factory()
+            ->has(Schedule::factory()->state([
+                'starts_at' => now()->startOfDay(),
+                'ends_at' => now()->addYear()->endOfDay(),
+            ]))->create();
+
+        $service = Service::factory()->create(['duration' => 30]);
+
+        $availability = (new ScheduleAvailability($employee, $service))->forPeriod(
+            now()->startOfDay(),
+            now()->endOfDay()
+        );
+
+        $this->assertInstanceOf(PeriodCollection::class, $availability);
+        $availabilityPeriod = $availability->current();
+        $dayName = strtolower(now()->format('l'));
+        $actualStartTime = $employee->schedules->first()->{$dayName . '_starts_at'};
+        $this->assertTrue($availabilityPeriod->startsAt(now()->setTimeFromTimeString($actualStartTime)));
     }
 }
